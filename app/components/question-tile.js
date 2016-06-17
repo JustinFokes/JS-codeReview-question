3e22d78d808c6d2d81ddf64f4608f1b081cd8c96
jf <j.k.fokes@gmail.com>
2016-06-17 13:22:12 -0700

    Implement service to store favorite questions

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,10 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	favoriteQuestion: Ember.inject.service(),
+	commentNotes: Ember.computed('question.comment', 'question.notes', function(){
+		return this.get('question.comment') +" " + 'Oh and by the way:' +" " + this.get('question.notes');
+	}),
+	
 	updateQuestionForm: false,
 	actions: {
 		update(question, params) {
 			this.sendAction('update', question, params);
+		},
+		addToList(question1) {
+			console.log(question1);
+			this.get('favoriteQuestion').add(question1);
 		}
 	}
 });
+
+
--- app/routes/question.js
+++ app/routes/question.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
 	model(params) {
 		return this.store.findRecord('question', params.question_id);
 	},
 	actions: {
+		
 		addAnswer3(params) {
 			var newAnswer = this.store.createRecord('answer', params);
 			var question = params.question;
@@ -13,6 +15,6 @@ export default Ember.Route.extend({
 				return question.save();
 			});
 			this.transitionTo('question', question.id);
-		}
+		},
 	}
 });
